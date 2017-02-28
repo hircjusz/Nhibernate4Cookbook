fc956d73e39a51f8749781f1fcb72905c70789c8
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-02-28 20:51:28 +0100

    Configuring NHibernate logging

--- NhConnector/ConfigByFNH/Program.cs
+++ NhConnector/ConfigByFNH/Program.cs
@@ -12,6 +12,8 @@ namespace ConfigByFNH
     {
         static void Main(string[] args)
         {
+            log4net.Config.XmlConfigurator.Configure();
+
             var config = MsSqlConfiguration
                 .MsSql2012
                 .ConnectionString(connstr => connstr.FromConnectionStringWithKey("db"))
