5c9793b46078f7649c29ef795f876f67b325d0fd
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-02-28 21:42:45 +0100

    Recipe 1 start

--- NhConnector/ConfigByFNH/Program.cs
+++ NhConnector/ConfigByFNH/Program.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eg.Core;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
 
 namespace ConfigByFNH
 {
@@ -12,16 +17,24 @@ namespace ConfigByFNH
     {
         static void Main(string[] args)
         {
-            log4net.Config.XmlConfigurator.Configure();
+            var recipe= new Recipe();
+            recipe.LogEnabled = true;
+            recipe.Run();
+        //    log4net.Config.XmlConfigurator.Configure();
 
-            var config = MsSqlConfiguration
-                .MsSql2012
-                .ConnectionString(connstr => connstr.FromConnectionStringWithKey("db"))
-                .AdoNetBatchSize(100);
+            //    var nhConfig = new Configuration().Configure();
+            //    var mapper = new ConventionModelMapper();
+            //    nhConfig.AddMapping(mapper.CompileMappingFor(new[] { typeof(TestClass) }));
+            //    var schemaExport = new SchemaExport(nhConfig);
+            //    schemaExport.Create(false, true);
+            //    //var config = MsSqlConfiguration
+            //    //    .MsSql2012
+            //    //    .ConnectionString(connstr => connstr.FromConnectionStringWithKey("db"))
+            //    //    .AdoNetBatchSize(100);
 
-            var nhConfig = Fluently.Configure()
-                .Database(config).BuildConfiguration();
-            var sessionFactory = nhConfig.BuildSessionFactory();
+            //    //var nhConfig = Fluently.Configure()
+            //    //    .Database(config).BuildConfiguration();
+            //    //var sessionFactory = nhConfig.BuildSessionFactory();
 
             Console.WriteLine("NHibernate configured fluently!");
             Console.ReadKey();
---
+++ NhConnector/ConfigByFNH/Recipe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eg.Core;
+using NH4CookbookHelpers;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace ConfigByFNH
+{
+    public class Recipe : BaseMappingRecipe
+    {
+
+        protected override void Configure(Configuration cfg)
+        {
+            cfg.AddAssembly(typeof(Product).Assembly);
+        }
+        protected override void AddInitialData(ISession session)
+        {
+            session.Save(new Product
+            {
+                Name = "Car",
+                Description = "A nice red car",
+                UnitPrice = 300
+            });
+        }
+    }
+}
